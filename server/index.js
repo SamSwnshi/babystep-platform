3551a554a78f3bc0cd04a019d9a0d3cc176776d2
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-06-24 00:23:44 +0530

    frontend url added

--- server/index.js
+++ server/index.js
@@ -15,9 +15,8 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE',],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
 }));
 
 app.use('/api',authRoutes)
